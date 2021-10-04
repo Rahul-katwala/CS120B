697976a3a0c84069e19d75578f3cf47d3c4777eb
Rahul-katwala <67291221+Rahul-katwala@users.noreply.github.com>
2021-10-03 21:36:40 -0700

    Update rkatw001_lab2_part1.c
    Completed part 1

--- rkatw001_lab2_part1.c
+++ rkatw001_lab2_part1.c
@@ -1,23 +1,42 @@
+/*      Author: rkatw001
+ *  Partner(s) Name: 
+ *      Lab Section:
+ *      Assignment: Lab #  Exercise #
+ *      Exercise Description: [optional - include for your own benefit]
+ *
+ *      I acknowledge all content contained herein, excluding template or example
+ *      code, is my own original work.
+ */
 #include <avr/io.h>
 #ifdef _SIMULATE_
-#include “simAVRHeader.h”
+#include "simAVRHeader.h"
 #endif
 
 int main(void) {
+    /* Insert DDR and PORT initializations */
 
-
-        DDRA= 0x00;
+    /* Insert your solution below */
+         DDRA= 0x00;
         PORTA = 0xFF;
         DDRB = 0xFF;
         PORTB = 0x00;
-
+        unsigned char pinAOne;
+        unsigned char pinAZero;
 
         while(1){
+                pinAOne = PINA & 0x02;
+                pinAZero = PINA & 0x01;
 
-                 if (((PINA & 0x01) == 0x01) && ((PINA & 0x02) != 0x02 )) {
+                 if (((pinAZero==0x01) && ((pinAOne) != 0x02 ))) {
                         PORTB = 0x01;
                 }
 
+        else {
+
+        PORTB = 0x00;
+        }
+
 
         }
+    return 1;
 }
